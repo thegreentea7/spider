1b08d6276140a5bdb79f19c7cc2acca21052b973
thegreentea7 <90368744+thegreentea7@users.noreply.github.com>
2026-09-01 12:17:00 +0500

    Delete scripts directory

--- scripts/node-cloudflare-loader.mjs
+++
@@ -1,9 +0,0 @@
-export async function resolve(specifier, context, nextResolve) {
-  if (specifier === "cloudflare:workers") {
-    return {
-      url: "data:text/javascript,export const env = {};",
-      shortCircuit: true,
-    };
-  }
-  return nextResolve(specifier, context);
-}
